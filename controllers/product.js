390b2b75bf28cb92ac63639198ed2ae8e632e185
Luis Gómez <l.gomeza@uniandes.edu.co>
2021-12-03 15:08:51 -0500

    Parcial 2 Finalizado

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,11 @@
 const data = require('../assets/data');
 
 function getProducts(query) {
+  if(query.q != null){
+    const productName = query.q.toLowerCase();
+    const newData = data.filter((product)=>product.name.toLowerCase().includes(productName));
+    return newData;
+  }
   return data;
 }
 
--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var productController = require('../controllers/product');
 
 /* GET products listing. Please establish connection with getProduct function from controllers/product.js  */
 router.get('/', function (req, res, next) {
   res.send(
-    'To implement. Please establish connection with getProduct function from controllers/product.js'
+    productController.getProducts(req.query)
   );
 });
 
